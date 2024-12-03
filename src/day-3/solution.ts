18eb3d7a233a84bcebd084f492e45c54408402f6
DrInfinite <work.naparajith@gmail.com>
2024-12-03 11:53:04 +0530

    feat: solved day 3 of aoc2024

---
+++ src/day-3/solution.ts
@@ -0,0 +1,53 @@
+export const expectedPartOneSampleOutput = '161';
+
+export function solvePartOne(input: string): string {
+  return input
+    .split('\n')
+    .map(
+      (line) =>
+        line
+          .match(/mul\((\d+),(\d+)\)/gi)
+          ?.map(
+            (line) =>
+              line
+                .match(/\d+/gi)
+                ?.map(Number)
+                .reduce((acc, curr) => (acc *= curr)),
+          )
+          .reduce((acc, curr) => (acc += curr)),
+    )
+    .reduce((acc, curr) => (acc += curr))
+    ?.toString() as string;
+}
+
+export const expectedPartTwoSampleOutput = '48';
+
+export function solvePartTwo(input: string): string {
+  return input
+    .split('\n')
+    .map(
+      (line) =>
+        line.match(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/gi) as RegExpMatchArray,
+    )
+    .toString()
+    .replace(/\),/gi, ')')
+    .replace(/don't\(\)(.*?)do\(\)/gi, '.')
+    .replace(/do\(\)/gi, '.')
+    .split('.')
+    .slice(1)
+    .map(
+      (line) =>
+        line
+          .match(/mul\((\d+),(\d+)\)/gi)
+          ?.map(
+            (value) =>
+              value
+                .match(/\d+/gi)
+                ?.map(Number)
+                .reduce((acc, curr) => acc * curr),
+          )
+          .reduce((acc, curr) => (acc += curr)),
+    )
+    .reduce((acc, curr) => (acc += curr))
+    ?.toString() as string;
+}
